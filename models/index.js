ce9312d80abb429a55942a71dac0d0f21efab9ce
PWadke <pwadke@kenmarkitan.com>
2023-04-10 13:37:06 +0530

    init commit

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const dbConfig = require("../configs/db.config");
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.user,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
