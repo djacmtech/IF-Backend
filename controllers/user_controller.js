885b4f0c516a6388a3968d48f21b5345ed13bf77
Medhashah03 <m0307shah@gmail.com>
2023-04-11 01:02:49 +0530

    added registeration in user_contr

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,15 +4,67 @@ const user = db.User; //user table from db
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+const mime = require('mime-types');
+
+const SALT = 10;
 //create or Register
 exports.create = async (req, res) => {
-    try{
-        const User = await user.create(req.body);     
-        res.send(User);
-    }catch(error){
-        console.log(error);
-    }
-};
+    try {
+        console.log(req.body);
+
+        const checkData = {
+          email: req.body.email,
+          sap : req.body.sap,
+          password: req.body.password,
+          resume : req.body.resume,
+          confirmPassword: req.body.confirmPassword,
+        };
+        const { error } = register.registerValidate(checkData);
+        if (error) {
+          console.log(error);
+          return res.status(400).send(error.details[0].message);
+        }
+
+        const User = await user.findOne({
+          where: {
+            sap: req.body.sap,
+          },
+        });
+        if (User) {
+          return res.status(400).send("user already registered.");
+        }
+
+        //validate pdf
+        const checkPDF = res.headers['content-type'];
+        const mimeType = mime.getType(checkPDF);
+        if (mimeType === 'application/pdf'){
+            //idk what to write here... acc to google continue aana chaiye
+        }
+        else{
+            return res.status(400).send("please enter valid file in pdf")
+        }
+
+        //yeh ig hona chaiye validate
+        if(password != confirmPassword){
+            console.log(error);
+            return res.status(400).send("password mismatch");
+        }
+        
+        //genSalt does hashing Salt is random value added to pwd
+        const salt = await bcrypt.genSalt(SALT);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //data to be saved
+        await Admin.create({
+          ...req.body,
+          password: hashedPassword,
+        });
+        res.send({ data: req.body });
+      } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+      }
+    };
 
 //login user
 exports.login = async(req,res) => {
