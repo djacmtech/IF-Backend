9ab8ae2a288fb8cf22c80626a74345ba6a074cc3
Medhashah03 <m0307shah@gmail.com>
2023-04-12 22:26:42 +0530

    added routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,13 @@
 module.exports = (app) => {
     const user = require("../controllers/user_controller");
-
+    const job = require("../controllers/jdesc_controller");
     const router = require("express").Router();
 
     router.post("/register", user.register);
     router.post("/login", user.login);
+    
+    router.post("/create", job.create);
+    router.get("/find_job", job.findAll);
 
     app.use("/api/acm-if", router);
 }
