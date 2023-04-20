954f41f2f3287b5c6f64723b0d409fb608f22338
Medhashah03 <m0307shah@gmail.com>
2023-04-20 21:34:29 +0530

    added receipt upload

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -3,7 +3,8 @@ const order = db.order;
 const cart = db.cart;
 const user = db.user;
 const job = db.job;
-
+const fs = require("fs");
+const { cloudinaryUploadReceipt } = require("../middleware/upload.cloudinary.js");
 exports.addOrder = async (req, res) => {
     try{
         const {userId} = req.body;
@@ -58,9 +59,26 @@ exports.addOrder = async (req, res) => {
         
         //calculate discount with creds used
         discount = (userData.acmMember - credits) * 50;
+        //payment receipt
 
+        if (!req.file) {
+            return res.status(400).send({
+              message: "Please upload the payment proof",
+            });
+          }
+
+        const localPath = `resources/receipts/${req.file.filename}`;
+        const uploadedReceipt = await cloudinaryUploadReceipt(localPath);
+        const receipturl = uploadedReceipt.url;
+        fs.unlink(localPath, (err) => {
+            if (err) {
+              console.log('Failed to delete local file:', err);
+            } else {
+              console.log('Local file deleted successfully.');
+            }
+          });
         //got jobs in cart, got the sumary, now create order
-
+        
         let newOrder = await order.create({
             userId: userId,
             totalPrice: totalPrice,
@@ -68,7 +86,8 @@ exports.addOrder = async (req, res) => {
             paymentMode: req.body.paymentMode,
             creditsUsed: userData.acmMember - credits,
             status: "paid",
-            paymentProof: "Medha Will do this"
+            //paymentProof: "Medha Will do this"
+            paymentProof : receipturl
         });
 
         //add jobs to order and remove from cart
