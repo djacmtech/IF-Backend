f938267aba4bdcfbb753e014a2255f2fab9a702f
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2023-04-20 22:37:12 +0530

    removed password complex

--- middleware/register.js
+++ middleware/register.js
@@ -7,6 +7,15 @@ exports.registerValidate = (data) => {
     const schema = joi.object().keys({
         email: joi.string().email().required().label('email'),
         password: joi.string().required().label('password'),
+        // password: passwordComplexity({
+        //     min: 8,
+        //     max: 26,
+        //     lowerCase: 1,
+        //     upperCase: 1,
+        //     numeric: 1,
+        //     symbol: 1,
+        //     requirementCount: 4,
+        // }),
         confirmPassword: joi.ref('password'),
     });
     return schema.validate(data);
